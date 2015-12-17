ac7408b27f52e57e3bfdf13bc01ed367a5a66a32
liwenzhu <wzli@kabaminc.com>
2015-12-17 20:24:27 +0800

    substring price count.

--- stock.js
+++ stock.js
@@ -4,6 +4,10 @@ var gbk = require('gbk');
 
 var WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL || 'https://hooks.slack.com/services/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';
 
+function devide100(s) {
+	return s.substring(0, s.length-2);
+}
+
 function checkStock(stock, cb) {
 	request.get({url:'http://hq.sinajs.cn/list=' + stock, encoding: null}, function(err, res, body) {
 		if (err) {
@@ -18,16 +22,16 @@ function checkStock(stock, cb) {
 		console.log('result:', result);
 		var result = '股票: ' + name + '\n';
 		result += '当前价格: ' + currentPrice + '\n';
-		result += '卖五:' + body[29] + '\t' + body[28] + '\n';
-		result += '卖四:' + body[27] + '\t' + body[26] + '\n';
-		result += '卖三:' + body[25] + '\t' + body[24] + '\n';
-		result += '卖二:' + body[23] + '\t' + body[22] + '\n';
-		result += '卖一:' + body[21] + '\t' + body[20] + '\n';
-		result += '买一:' + body[11] + '\t' + body[10] + '\n';
-		result += '买二:' + body[13] + '\t' + body[12] + '\n';
-		result += '买三:' + body[15] + '\t' + body[14] + '\n';
-		result += '买四:' + body[17] + '\t' + body[16] + '\n';
-		result += '买五:' + body[19] + '\t' + body[18] + '\n';
+		result += '卖五:' + body[29] + '\t' + devide100(body[28]) + '\n';
+		result += '卖四:' + body[27] + '\t' + devide100(body[26]) + '\n';
+		result += '卖三:' + body[25] + '\t' + devide100(body[24]) + '\n';
+		result += '卖二:' + body[23] + '\t' + devide100(body[22]) + '\n';
+		result += '卖一:' + body[21] + '\t' + devide100(body[20]) + '\n';
+		result += '买一:' + body[11] + '\t' + devide100(body[10]) + '\n';
+		result += '买二:' + body[13] + '\t' + devide100(body[12]) + '\n';
+		result += '买三:' + body[15] + '\t' + devide100(body[14]) + '\n';
+		result += '买四:' + body[17] + '\t' + devide100(body[16]) + '\n';
+		result += '买五:' + body[19] + '\t' + devide100(body[18]) + '\n';
 		result += '++++++++++++++++++++\n';
 		console.log('before return :', result);
 		return cb(null, result);
