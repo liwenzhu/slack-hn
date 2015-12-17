3d63bf7e64583068bed77818d56717615ee476ce
liwenzhu <wzli@kabaminc.com>
2015-12-17 16:15:07 +0800

    remove interval.

--- hnbot.js
+++ hnbot.js
@@ -8,50 +8,42 @@ var timer = null;
 
 module.exports = function (req, res, next) {
 
-	var getNews = function(){
-		request('http://node-hnapi.herokuapp.com/news', function(err, response, items) {
-			if (err || response.statusCode != 200) {
-				return res.status(500).end();
-			}
+	request('http://node-hnapi.herokuapp.com/news', function(err, response, items) {
+		if (err || response.statusCode != 200) {
+			return res.status(500).end();
+		}
 
-			items = JSON.parse(items);
+		items = JSON.parse(items);
 
-			var botPayload = 'Current Hacker News homepage:\n';
+		var botPayload = 'Current Hacker News homepage:\n';
 
-			var index = 0;
-			async.forEach(items, function(item, cb) {
-				botPayload += '<%url%|%rank%. %title%>\n'
-                    .replace('%url%', item.url)
-                    .replace('%rank%', index + 1)
-                    .replace('%title%', item.title);
-				index++;
-				cb();
-			}, function(err) {
+		var index = 0;
+		async.forEach(items, function(item, cb) {
+			botPayload += '<%url%|%rank%. %title%>\n'
+                .replace('%url%', item.url)
+                .replace('%rank%', index + 1)
+                .replace('%title%', item.title);
+			index++;
+			cb();
+		}, function(err) {
 
-				var payload = {
-					text: botPayload,
-					username: 'HackerNews',
-					channel: req.query.channel_id
-				};
+			var payload = {
+				text: botPayload,
+				username: 'HackerNews',
+				channel: req.query.channel_id
+			};
 
-				request.post({
-					url: WEBHOOK_URL,
-					body: JSON.stringify(payload)
+			request.post({
+				url: WEBHOOK_URL,
+				body: JSON.stringify(payload)
 
-				}, function(err, resp, data) {
-					var now = new moment();
-					now.utcOffset(480); // Asia/Shanghai
-					if (new Date().getHours() < 18) {
-						setTimeout(getNews, 1000 * 60 * 60); // one hour later check new again
-					}
-					if (err) {
-						return res.status(500).end();
-					}
-					return res.status(200).end();
-				});
+			}, function(err, resp, data) {
+				if (err) {
+					return res.status(500).end();
+				}
+				return res.status(200).end();
 			});
 		});
-	};
+	});
 
-	getNews();
 }
